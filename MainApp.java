9ee0cdc7d47706fb7e8bd112d0b943274ed9fa7b
VanExcel Ursad <vanexcel777.vanexelursad@gmail.com>
2022-11-24 11:19:11 +0400

    ajout de fichier java

---
+++ MainApp.java
@@ -0,0 +1,17 @@
+class MainApp
+// class MainApp c'est la definition de la classe qui fait reference a la fichier source
+
+
+{
+    public static void  main (String [] args )
+  /*main c'est la fonction de depart  ou methode  
+    args c'est une parametre une signature c'est obligatoire 
+    void c'est le retour  
+
+  */
+
+    {
+         System.out.println("Bonjour Java ! :) ");
+         //System est une classe native de java donc en majuscule
+    }
+}
